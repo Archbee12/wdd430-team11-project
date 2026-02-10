09718f3aec73e7f536397ef061772434ad09bbfd
unknown <debbieokon32@gmail.com>
2026-02-10 06:09:22 +0100

    successfully seeded database

--- team11handcrafted/src/app/ui/dashboard/artisan-cards.tsx
+++ team11handcrafted/src/app/ui/dashboard/artisan-cards.tsx
@@ -1,24 +1,3 @@
-// import ImageCard from '@/app/ui/dashboard/artisan-cards';
-
-// export default function ArtisanCards() {
-//   return (
-//     <>
-//       <ImageCard
-//         imageSrc="/images/artisan1.jpg"
-//         title="Amina Bello"
-//         subtitle="Textile Artisan"
-//         meta="12 artisans"
-//       />
-
-//       <ImageCard
-//         imageSrc="/images/artisan2.jpg"
-//         title="Kofi Mensah"
-//         subtitle="Wood Crafts"
-//         meta="8 artisans"
-//       />
-//     </>
-//   );
-// }
 
 type ImageCardProps = {
   imageSrc: string;
